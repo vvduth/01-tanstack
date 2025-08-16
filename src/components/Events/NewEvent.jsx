84cc0fef63eb7463cf15628943af56e7a12bca8b
vvduth <ducthai060501@gmail.com>
2025-08-16 18:00:57 +0300

    refactor: update event fetching logic and improve loading state handling

--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -1,5 +1,5 @@
 import { Link, useNavigate } from 'react-router-dom';
-
+import {useMutation} from "@tanstack/react-query"
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
 
