6f47242b66ae50d417004837bfd9ead9e921ccee
vvduth <ducthai060501@gmail.com>
2025-08-18 15:46:09 +0300

    refactor: enhance event details fetching and display logic, add new event and image data

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,8 +1,24 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Link, Outlet, useParams } from 'react-router-dom';
 
 import Header from '../Header.jsx';
-
+import { fetchEvent } from '../../utils/http.js';
+import {useQuery} from "@tanstack/react-query"
 export default function EventDetails() {
+
+  const {id} = useParams();
+  const {data: event, isLoading, isError} = useQuery({
+    queryKey: ['event', {id}],
+    queryFn: ({signal}) => fetchEvent({id, signal})
+  });
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (isError) {
+    return <p>Error loading event details</p>;
+  }
+
   return (
     <>
       <Outlet />
@@ -11,25 +27,27 @@ export default function EventDetails() {
           View all Events
         </Link>
       </Header>
-      <article id="event-details">
+      {event && (
+        <article id="event-details">
         <header>
-          <h1>EVENT TITLE</h1>
+          <h1>{event.title}</h1>
           <nav>
             <button>Delete</button>
             <Link to="edit">Edit</Link>
           </nav>
         </header>
         <div id="event-details-content">
-          <img src="" alt="" />
+          <img src={`http://localhost:3000/${event.image}`} alt={event.title}/>
           <div id="event-details-info">
             <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
+              <p id="event-details-location">{event.location}</p>
+              <time dateTime={`Todo-DateT$Todo-Time`}>{event.date} @ {event.time}</time>
             </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
+            <p id="event-details-description">{event.description}</p>
           </div>
         </div>
       </article>
+      )}
     </>
   );
 }
--- src/utils/http.js
+++ src/utils/http.js
@@ -62,3 +62,36 @@ export async function fetchSelectableImages({signal}) {
   return images;
   
 }
+
+export async function fetchEvent({id,signal}) {
+  const response = await fetch(`http://localhost:3000/events/${id}`, {
+    signal
+  });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while fetching the event');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { event } = await response.json();
+
+  return event;
+}
+
+export async function deleteEvent({id, signal}) {
+  const response = await fetch(`http://localhost:3000/events/${id}`, {
+    method: 'DELETE',
+    signal
+  });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while deleting the event');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  return null;
+}
