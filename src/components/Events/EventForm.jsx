b215016cbe4c76aeaec194350e61069ef1a57fbc
vvduth <ducthai060501@gmail.com>
2025-08-18 09:19:41 +0300

    refactor: add selectable images fetching logic and integrate image picker in EventForm component

--- src/components/Events/EventForm.jsx
+++ src/components/Events/EventForm.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react';
-
+import {useQuery} from "@tanstack/react-query"
 import ImagePicker from '../ImagePicker.jsx';
-
+import { fetchSelectableImages } from '../../utils/http.js';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 export default function EventForm({ inputData, onSubmit, children }) {
   const [selectedImage, setSelectedImage] = useState(inputData?.image);
 
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ['events-images'],
+    queryFn: fetchSelectableImages
+  })
   function handleSelectImage(image) {
     setSelectedImage(image);
   }
@@ -30,13 +35,20 @@ export default function EventForm({ inputData, onSubmit, children }) {
         />
       </p>
 
-      <div className="control">
+    {isPending && <p>Loading images...</p>}
+    {isError && <ErrorBlock
+      title="Failed to load images"
+      message={error?.info?.message || 'Something went wrong'}
+    />}
+      {data && (
+        <div className="control">
         <ImagePicker
-          images={[]}
+          images={data}
           onSelect={handleSelectImage}
           selectedImage={selectedImage}
         />
       </div>
+      )}
 
       <p className="control">
         <label htmlFor="description">Description</label>
--- src/utils/http.js
+++ src/utils/http.js
@@ -41,3 +41,21 @@ export async function createNewEvent(eventData) {
 
   return event;
 }
+
+export async function fetchSelectableImages({signal}) {
+  const response = await fetch('http://localhost:3000/images', {
+    signal
+  });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while fetching images');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { images } = await response.json();
+
+  return images;
+  
+}
