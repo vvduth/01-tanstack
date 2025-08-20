67fe9d2adf03d04c47f88b80c4e3153137befade
vvduth <ducthai060501@gmail.com>
2025-08-20 11:19:34 +0300

    refactor: enhance event deletion functionality and improve event details rendering

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,14 +1,25 @@
-import { Link, Outlet, useParams } from 'react-router-dom';
-
-import Header from '../Header.jsx';
-import { fetchEvent } from '../../utils/http.js';
-import {useQuery} from "@tanstack/react-query"
+import { Link, Outlet, useNavigate, useNavigation, useParams } from "react-router-dom";
+import { queryClient } from "../../utils/http.js";
+import Header from "../Header.jsx";
+import { fetchEvent, deleteEvent } from "../../utils/http.js";
+import { useMutation, useQuery } from "@tanstack/react-query";
 export default function EventDetails() {
-
-  const {id} = useParams();
-  const {data: event, isLoading, isError} = useQuery({
-    queryKey: ['event', {id}],
-    queryFn: ({signal}) => fetchEvent({id, signal})
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const {
+    data: event,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["event", { id }],
+    queryFn: ({ signal }) => fetchEvent({ id, signal }),
+  });
+  const { mutate } = useMutation({
+    mutationFn: ({ id }) => deleteEvent({ id }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["events"] });
+      navigate("/events");
+    },
   });
 
   if (isLoading) {
@@ -18,6 +29,10 @@ export default function EventDetails() {
   if (isError) {
     return <p>Error loading event details</p>;
   }
+
+  const handleDelete = () => {
+    mutate({ id });
+  };
 
   return (
     <>
@@ -29,24 +44,29 @@ export default function EventDetails() {
       </Header>
       {event && (
         <article id="event-details">
-        <header>
-          <h1>{event.title}</h1>
-          <nav>
-            <button>Delete</button>
-            <Link to="edit">Edit</Link>
-          </nav>
-        </header>
-        <div id="event-details-content">
-          <img src={`http://localhost:3000/${event.image}`} alt={event.title}/>
-          <div id="event-details-info">
-            <div>
-              <p id="event-details-location">{event.location}</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>{event.date} @ {event.time}</time>
+          <header>
+            <h1>{event.title}</h1>
+            <nav>
+              <button onClick={handleDelete}>Delete</button>
+              <Link to="edit">Edit</Link>
+            </nav>
+          </header>
+          <div id="event-details-content">
+            <img
+              src={`http://localhost:3000/${event.image}`}
+              alt={event.title}
+            />
+            <div id="event-details-info">
+              <div>
+                <p id="event-details-location">{event.location}</p>
+                <time dateTime={`Todo-DateT$Todo-Time`}>
+                  {event.date} @ {event.time}
+                </time>
+              </div>
+              <p id="event-details-description">{event.description}</p>
             </div>
-            <p id="event-details-description">{event.description}</p>
           </div>
-        </div>
-      </article>
+        </article>
       )}
     </>
   );
